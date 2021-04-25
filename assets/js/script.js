cfb4c06e1bea7dcd91db899b45f46eab757aefda
Bobby T <bob.tarino@gmail.com>
2021-04-25 18:27:41 -0500

    added temp wind and humidity from api response list

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,39 @@ $("#search-button").click(function(event) {
     //prevents page from reloading
     event.preventDefault();
       //grab value of input field
-      var input = $("#user-input").val(); 
-      var searchURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&units=imperial&appid=6658bc2aa5781c0084a75b25ca5febd7"
+      var inputCity = $("#user-input").val(); 
+      var searchURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&units=imperial&appid=6658bc2aa5781c0084a75b25ca5febd7"
       // fetch data
       // API request
-      fetch(
-        searchURL
-          // Add the rating parameter
-          // YOUR CODE HERE
-          //
-          // Remember to add your API key
-      )
+      fetch(searchURL)
         .then(function(response) {
           return response.json();
         })
         .then(function(response) {
           
-          for(var i = 0; i < response.list.length; i++) {
-           console.log(response.list[i].dt_txt);   
+            for(var i = 0; i < response.list.length; i++) {
+            console.log(response.list[4]);  
+            // Day 1
+            var tempText1 =  JSON.stringify(response.list[4].main.temp);
+            $(".temp1").text("Temp: " + tempText1 + " ℉");
+            var windText1 =  JSON.stringify(response.list[4].wind);
+            $(".wind1").text("Wind: " + windText1);
+            var humidText1 =  JSON.stringify(response.list[4].main.humidity);
+            $(".humidity1").text("Humid: " + humidText1 + " %");
+
+
           }
         });
     
 })
+
  
 
 
 //fecth data 
+
+
+
+
+
+
