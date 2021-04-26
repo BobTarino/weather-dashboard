d3961c01e1697cf24d580c3acf7f56cf64f53eea
Bobby T <bob.tarino@gmail.com>
2021-04-25 20:50:51 -0500

    installed current weather api and created card

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,23 @@ $("#search-button").click(function(event) {
       //grab value of input field
       var inputCity = $("#user-input").val(); 
       var searchURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&units=imperial&appid=6658bc2aa5781c0084a75b25ca5febd7"
+      var currentURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&units=imperial&appid=6658bc2aa5781c0084a75b25ca5febd7"
       // fetch data
-      // API request
+      // API request for current weather
+      fetch(currentURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response);
+            var curTempText =  JSON.stringify(response.main.temp);
+            $(".current-temp").text("Temp: " + curTempText + " ℉");
+            var curWindText =  JSON.stringify(response.wind.speed);
+            $(".current-wind").text("Wind: " + curWindText + " MPH");
+            var curHumidText =  JSON.stringify(response.main.humidity);
+            $(".current-humidity").text("Humid: " + curHumidText + " %"); 
+        });
+      // API request for 5 day forecast
       fetch(searchURL)
         .then(function(response) {
           return response.json();
@@ -17,14 +32,14 @@ $("#search-button").click(function(event) {
         .then(function(response) {
           
             for(var i = 0; i < response.list.length; i++) {
-            console.log(response.list[i].dt_txt);  
+            // console.log(response.list[i].dt_txt);  
             // Day 1
             var dateText1 =  JSON.stringify(response.list[3].dt_txt);
             $(".date1").text(dateText1)
             var tempText1 =  JSON.stringify(response.list[3].main.temp);
             $(".temp1").text("Temp: " + tempText1 + " ℉");
-            var windText1 =  JSON.stringify(response.list[3].wind);
-            $(".wind1").text("Wind: " + windText1);
+            var windText1 =  JSON.stringify(response.list[3].wind.speed);
+            $(".wind1").text("Wind: " + windText1 + " MPH");
             var humidText1 =  JSON.stringify(response.list[3].main.humidity);
             $(".humidity1").text("Humid: " + humidText1 + " %");
             // Day 2
@@ -32,8 +47,8 @@ $("#search-button").click(function(event) {
             $(".date2").text(dateText2);
             var tempText2 =  JSON.stringify(response.list[11].main.temp);
             $(".temp2").text("Temp: " + tempText2 + " ℉");
-            var windText2 =  JSON.stringify(response.list[11].wind);
-            $(".wind2").text("Wind: " + windText2);
+            var windText2 =  JSON.stringify(response.list[11].wind.speed);
+            $(".wind2").text("Wind: " + windText2 + " MPH");
             var humidText2 =  JSON.stringify(response.list[11].main.humidity);
             $(".humidity2").text("Humid: " + humidText2 + " %");
             // Day 3
@@ -41,8 +56,8 @@ $("#search-button").click(function(event) {
             $(".date3").text(dateText3);
             var tempText3 =  JSON.stringify(response.list[19].main.temp);
             $(".temp3").text("Temp: " + tempText3 + " ℉");
-            var windText3 =  JSON.stringify(response.list[19].wind);
-            $(".wind3").text("Wind: " + windText3);
+            var windText3 =  JSON.stringify(response.list[19].wind.speed);
+            $(".wind3").text("Wind: " + windText3 + " MPH");
             var humidText3 =  JSON.stringify(response.list[19].main.humidity);
             $(".humidity3").text("Humid: " + humidText3 + " %");
             // Day 4
@@ -50,8 +65,8 @@ $("#search-button").click(function(event) {
             $(".date4").text(dateText4);
             var tempText4 =  JSON.stringify(response.list[27].main.temp);
             $(".temp4").text("Temp: " + tempText4 + " ℉");
-            var windText4 =  JSON.stringify(response.list[27].wind);
-            $(".wind4").text("Wind: " + windText4);
+            var windText4 =  JSON.stringify(response.list[27].wind.speed);
+            $(".wind4").text("Wind: " + windText4 + + " MPH");
             var humidText4 =  JSON.stringify(response.list[27].main.humidity);
             $(".humidity4").text("Humid: " + humidText4 + " %");
             // Day 5
@@ -59,12 +74,10 @@ $("#search-button").click(function(event) {
             $(".date5").text(dateText5);
             var tempText5 =  JSON.stringify(response.list[35].main.temp);
             $(".temp5").text("Temp: " + tempText5 + " ℉");
-            var windText5 =  JSON.stringify(response.list[35].wind);
-            $(".wind5").text("Wind: " + windText5);
+            var windText5 =  JSON.stringify(response.list[35].wind.speed);
+            $(".wind5").text("Wind: " + windText5 + " MPH");
             var humidText5 =  JSON.stringify(response.list[35].main.humidity);
             $(".humidity5").text("Humid: " + humidText5 + " %");
-
-
           }
         });
     
